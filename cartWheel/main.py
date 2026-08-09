daecea2f08e6996e34c6b42933aca986f1e0ffc0
pranavlaji <pranavlaji@gmail.com>
2026-08-09 19:51:53 +0530

    cartpole w/ dqn

--- cartWheel/main.py
+++ cartWheel/main.py
@@ -1,114 +1,156 @@
 import gymnasium as gym
 import numpy as np
 import random
+import time
 
-env = gym.make("CartPole-v1", render_mode="human")
-
-obs, info = env.reset()
+env = gym.make("CartPole-v1")
 
 epsilon = 0.1
-alpha = 0.9
+alpha = 0.05
 gamma = 0.9
 
-print("Initial observation:", obs)
+Q = np.zeros((10, 10, 10, 10, 2))
 
-Q = np.zeros((10,10,10,10,2))
+
+def bucketize(value, min_val, max_val, num_buckets):
+    normalized = (value - min_val) / (max_val - min_val)
+
+    bucket = int(normalized * num_buckets)
+
+    bucket = max(0, bucket)
+    bucket = min(bucket, num_buckets - 1)
+
+    return bucket
+
+
+def get_state(obs):
+    cart_pos = bucketize(obs[0], -1.0, 1.0, 10)
+    cart_vel = bucketize(obs[1], -4.0, 2.0, 10)
+    pole_angle = bucketize(obs[2], -0.51, 0.51, 10)
+    pole_vel = bucketize(obs[3], -3.5, 5.5, 10)
+
+    return (
+        cart_pos,
+        cart_vel,
+        pole_angle,
+        pole_vel
+    )
+
 
 def choose_action(state):
-    p1 ,p2, p3, p4 = state
+
     if random.random() < epsilon:
         return random.randint(0, 1)
 
-    return np.argmax(Q[p1,p2,p3,p4])
+    return np.argmax(Q[state])
 
-def poleAngle(polAngleVal,polAngleMin_val,polAngleMax_val,polAngleNum_buckets):
-        normalized = (
-            polAngleVal - polAngleMin_val
-        ) / (
-            polAngleMax_val - polAngleMin_val
-        )
 
-        bucket = int(
-            normalized * polAngleNum_buckets
-        )
-        return bucket
+def updateQ(curState, nextState, action, reward, done):
 
-def cartPosition(cartPosVal,cartPosMin_val,cartPosMax_val,cartPosNum_buckets):
-        normalized = (
-            cartPosVal - cartPosMin_val
-        ) / (
-            cartPosMax_val - cartPosMin_val
-        )
+    if done:
+        target = reward
+    else:
+        target = reward + gamma * np.max(Q[nextState])
 
-        bucket = int(
-            normalized * cartPosNum_buckets
-        )
-        return bucket
-
-def cartVelocity(cartVelVal,cartVelMin_val,cartVelMax_val,cartVelNum_buckets):
-        normalized = (
-            cartVelVal - cartVelMin_val
-        ) / (
-            cartVelMax_val - cartVelMin_val
-        )
-
-        bucket = int(
-            normalized * cartVelNum_buckets
-        )
-        return bucket
-
-def poleVelocity(polVelVal,polVelMin_val,polVelMax_val,polVelNum_buckets):
-        normalized = (
-            polVelVal - polVelMin_val
-        ) / (
-            polVelMax_val - polVelMin_val
-        )
-
-        bucket = int(
-            normalized * polVelNum_buckets
-        )
-        return bucket
-
-def updateQ(curState,nextState,action,reward):
-  
-    best_next_q = np.max(Q[nextState])
-
-    Q[curState, action] = (
-        Q[curState, action]
-        + alpha * (
-            reward
-            + gamma * best_next_q
-            - Q[curState, action]
-        )
+    Q[curState][action] += (
+        alpha * (target - Q[curState][action])
     )
 
 
-obs = [poleAngle(obs[2],-0.2,0.2,9),
-poleVelocity(obs[3],-0.10,0.10,9),
-cartVelocity(obs[1],-1.0,1.0,9),
-cartPosition(obs[0],-1.0,1.0,9)]
+NUM_EPISODES = 50000
 
-for step in range(20):
-    state = obs
+cart_positions = []
+cart_velocities = []
+pole_angles = []
+pole_velocities = []
+rewards = []
+
+for episode in range(NUM_EPISODES):
+
+    obs, info = env.reset()
+    cart_positions.append(obs[0])
+    cart_velocities.append(obs[1])
+    pole_angles.append(obs[2])
+    pole_velocities.append(obs[3])   
+
+    state = get_state(obs)
+
+    total_reward = 0
+
+    while True:
+
+        action = choose_action(state)
+
+        newObs, reward, terminated, truncated, info = env.step(action)
+        cart_positions.append(newObs[0])
+        cart_velocities.append(newObs[1])
+        pole_angles.append(newObs[2])
+        pole_velocities.append(newObs[3])
+        reward = reward - 2 * abs(newObs[2])
+
+        nextState = get_state(newObs)
+
+        done = terminated or truncated
+
+        updateQ(
+            state,
+            nextState,
+            action,
+            reward,
+            done
+        )
+        state = nextState
+
+        total_reward += reward
+
+        if terminated or truncated:
+            break
+    rewards.append(total_reward)
+    if episode % 50 == 0:
+        print(
+            f"Episode {episode} | "
+            f"Reward: {total_reward}"
+        )
+    if episode % 100 == 0:
+        print(
+            f"Episode {episode} | "
+            f"Avg Reward: {np.mean(rewards[-100:]):.2f}"
+        )
+    epsilon = max(0.1, epsilon * 0.999)
+
+print("\nTraining Complete\n")
+
+print("\nObserved Ranges:")
+print("Cart Position :", min(cart_positions), max(cart_positions))
+print("Cart Velocity :", min(cart_velocities), max(cart_velocities))
+print("Pole Angle    :", min(pole_angles), max(pole_angles))
+print("Pole Velocity :", min(pole_velocities), max(pole_velocities))
+env = gym.make("CartPole-v1", render_mode="human")
+
+epsilon = 0
+
+obs, info = env.reset()
+
+state = get_state(obs)
+
+print("\nTesting Learned Policy\n")
+
+
+while True:
+
     action = choose_action(state)
 
-    newObs, reward, terminated, truncated, info = env.step(action)
+    obs, reward, terminated, truncated, info = env.step(action)
 
-    newState = [poleAngle(newObs[2],-0.2,0.2,9),
-    poleVelocity(newObs[3],-0.10,0.10,9),
-    cartVelocity(newObs[1],-1.0,1.0,9),
-    cartPosition(newObs[0],-1.0,1.0,9)]
-  
-    updateQ(state,newState,action,reward)
-    state = newState
+    state = get_state(obs)
+
     print(
-        f"Step {step+1} | "
-        f"Obs: {obs} | "
-        f"Action: {action} |" 
-        f"Reward: {reward}"
+        f"State: {state} | "
+        f"Action: {action}"
     )
     if terminated or truncated:
-        print("Episode ended")
+        print("Episode Ended")
         break
+
 
 env.close()
